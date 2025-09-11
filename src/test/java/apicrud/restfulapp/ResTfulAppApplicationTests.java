dedf1932a9b32eb6e31acd96fd7c0bd698abf8e7
Al-issawi <md.alisawi@gmail.com>
2025-09-11 14:49:23 +0200

    main Sitting maven

---
+++ src/test/java/apicrud/restfulapp/ResTfulAppApplicationTests.java
@@ -0,0 +1,13 @@
+package apicrud.restfulapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
