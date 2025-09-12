01464c53bba09ed364c275875cae604e313f1db1
Al-issawi <md.alisawi@gmail.com>
2025-09-12 16:16:25 +0200

    Creating Entinty Class, DDBB ,Repo, Service,Controlle for table products. 2.Implintations for the CRUD  operations methods. between Repo and service for all crud operations.

---
+++ src/main/java/apicrud/restfulapp/controllers/ProductControlller.java
@@ -0,0 +1,17 @@
+package apicrud.restfulapp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductControlller {
+
+    /*TODO : Add the services to the controller
+      TODO Create a service to get all products
+      TODO get product by id,
+      TODO create product,
+      TODO update product,
+      TODO delete product */
+
+    }
